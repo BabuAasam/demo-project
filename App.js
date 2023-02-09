7643d052596b4580db20676ad79394339dd637fe
Macbook <macbook@Macbooks-MacBook-Air.local>
2023-02-09 17:31:12 +0530

    demo-project

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Route from './src/navigation/Route';
+
+const App = () => {
+  return (
+    <View style={{flex: 1}}>
+      <Route />
+    </View>
+  );
+};
+
+export default App;
